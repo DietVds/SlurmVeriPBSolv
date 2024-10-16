9903bbbf3bdcde16d475c09ed2c43f87faf56907
DietVds <vandesande.dieter@gmail.com>
2024-10-16 14:14:19 +0200

    Remove results when cleanup_files is run

--- cleanup_files.sh
+++ cleanup_files.sh
@@ -19,11 +19,14 @@ if [ -z "$1" ]; then
     rm -r $loc_outputs/*
     rm -r $loc_proofs/*
     rm -r $loc_running_scripts/*
+    rm -r $loc_results/*
     rm runtask.log
 else 
     rm -r $loc_outputs/$1
     rm -r $loc_proofs/$1
     rm -r $loc_running_scripts/$1
+    rm -r $loc_results/$1
+    rm -r $loc_results/resultheader_$1.txt
     rm $loc_running_scripts/slurm_run_$1.pbs
     rm runtask_$1.log
 fi
