a7ede01a8b659b8c359ab662170415ce05a7872b
DietVds <vandesande.dieter@gmail.com>
2024-10-16 10:52:37 +0200

    Make locations in config.sh more flexible

--- config.sh
+++ config.sh
@@ -1,13 +1,12 @@
 HERE=$(pwd)
 
+loc_software=$VSC_DATA_VO_USER
 loc_tools=$VSC_DATA_VO_USER/SlurmVeriPBSolv/tools
 loc_scripts=$VSC_DATA_VO_USER/SlurmVeriPBSolv
 
-loc_instances_archive=$VSC_DATA_VO_USER/mse22-complete-weighted.zip
-loc_instances=$VSC_SCRATCH_VO_USER/pacose-analysis/instances2022
-loc_running_scripts=$VSC_SCRATCH_VO_USER/pacose-analysis/running_scripts
-loc_bin=$VSC_SCRATCH_VO_USER/pacose-analysis/bin
-loc_outputs=$VSC_SCRATCH_VO_USER/pacose-analysis/outputs
-loc_results=$VSC_DATA_VO_USER/pacose-analysis/results
-loc_proofs=$VSC_SCRATCH_VO_USER/pacose-analysis/proofs
-# loc_verification_failed=$VSC_DATA_VO_USER/verification_failed
+loc_instances=$VSC_SCRATCH_VO_USER/instances
+loc_running_scripts=$VSC_SCRATCH_VO_USER/running_scripts
+loc_bin=$VSC_SCRATCH_VO_USER/bin
+loc_outputs=$VSC_SCRATCH_VO_USER/outputs
+loc_results=$VSC_DATA_VO_USER/results
+loc_proofs=$VSC_SCRATCH_VO_USER/proofs
--- unpack_instances.sh
+++ unpack_instances.sh
@@ -2,6 +2,34 @@
 
 source config.sh
 
+usage () {
+cat << EOF
+usage: ./run_experiments.sh <instances_archive> <out>
+
+where 
+
+   <instances_archive>  is an archive containing instances
+   <out>                is the folder where the instances needs to be placed
+
+call ./run_experiments.sh --help to show this help page.
+EOF
+exit 0
+}
+
+if [ -z $1 ] || [ -z $2 ]; then 
+	echo "ERROR: Argument missing. Correct usage: "
+	usage
+	exit 0
+fi
+
+if [[ "$1" == "--help" ]]; then 
+	usage
+	exit 0
+fi
+
+loc_instances_archive=$1
+loc_instances=$2
+
 unzip -j $loc_instances_archive -d $loc_instances 
 
 cd $loc_instances
