89e1b4c2c722bfac036f99cb5eef60bf70c54576
DietVds <vandesande.dieter@gmail.com>
2024-10-17 10:00:46 +0200

    update -all option to cleanup_files

--- cleanup_files.sh
+++ cleanup_files.sh
@@ -2,20 +2,28 @@ source config.sh
 
 usage () {
     cat << EOF
-usage: ./cleanup_files.sh <experiment-name> 
+usage: ./cleanup_files.sh <experiment-name> | --help | --all
 
 where 
 
 <experiment-name>	name of the experiment. If empty, all files in $loc_outputs, $loc_proofs, $loc_results and $loc_running_scripts will be removed. Otherwise only folders with the experiment-name will be removed.
+--help              print this help page
+--all               cleanup files from all experiments
 EOF
 }
 
-if [[ "$1" == "--help" ]]; then 
+if [ -z "$1" ]; then 
+    echo "ERROR: first argument missing." 
+    usage
+    exit 0
+fi
+
+if [ "$1" == "--help" ]; then 
     usage 
     exit 0
 fi
 
-if [ -z "$1" ]; then
+if [ "$1" ==  "--all"]; then
     rm -r $loc_outputs/*
     rm -r $loc_proofs/*
     rm -r $loc_running_scripts/*
