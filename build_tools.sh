c7bc174ec55ea76be3554e39304c7f3a98f4260a
DietVds <vandesande.dieter@gmail.com>
2024-10-18 11:30:18 +0200

    Create seperate script for tool building

---
+++ build_tools.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source helper/config.sh
+source helper/load_modules.sh
+
+#Building runlim
+rm $loc_bin/runlim
+echo Building runlim...
+cd $loc_tools
+tar -xf runlim-1.10.tar.gz
+cd runlim-1.10
+./configure.sh && make
+mv runlim $loc_bin
+cd $loc_tools
+rm -rf runlim-1.10
+echo Finished building runlim
+
+#Building VeriPB if necessary
+rm -rf $loc_bin/pyenv
+# virtualenv --system-site-packages $loc_bin/pyenv
+virtualenv $loc_bin/pyenv
+source $loc_bin/pyenv/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade setuptools
+cd $loc_tools
+unzip VeriPB.zip
+python3 -m pip install VeriPB-master/
+rm -rf VeriPB-master
---
+++ build_tools_on_partition.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+usage () {
+    cat << EOF
+usage: ./build_tools_on_partition.sh <option>
+
+where <option> can be 
+
+<partition>	        the partition on which the tools need to be built.
+--help              print this help page
+EOF
+}
+
+if [ -z "$1" ]; then 
+    echo "ERROR: first argument missing." 
+    usage
+    exit 0
+fi
+
+if [ "$1" == "--help" ]; then 
+    usage 
+    exit 0
+fi
+
+sbatch --job-name=BUILD --ntasks=1 --partition=$1 --cpus-per-task=1  --mail-type=$MAILTYPE build_tools.sh
