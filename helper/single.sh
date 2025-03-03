743908a142a3ed1dd24e21349645923ce00f77d7
DietVds <vandesande.dieter@gmail.com>
2025-03-03 10:45:41 +0100

    Copy instance to /dev/shm

--- helper/single.sh
+++ helper/single.sh
@@ -39,11 +39,12 @@ res_mem_proofchecker=""
 res_proofcheck_succeeded=""
 
 # Read the instance once, to not have overhead in the parsing for the first solver call.
-cat $instances/${filename}.${extension} > /dev/null
+instance=/dev/shm/${filename}.${extension}
+cp $instance $instance
 
 if [ -n "$script_without_PL" ]; then
   # run solver without prooflogging
-  $loc_bin/runlim -r $TIMEOUT_SOLVER -s $MEMOUT_SOLVER -o $loc_outputs/$experiment_name/${filename}_vanilla_out.txt $script_without_PL $instances/${filename}.${extension} > $loc_outputs/$experiment_name/${filename}_vanilla_solveroutput.txt 2>&1
+  $loc_bin/runlim -r $TIMEOUT_SOLVER -s $MEMOUT_SOLVER -o $loc_outputs/$experiment_name/${filename}_vanilla_out.txt $script_without_PL $instance > $loc_outputs/$experiment_name/${filename}_vanilla_solveroutput.txt 2>&1
   
   # extract results
   res_runtime_withoutPL=$(cat $loc_outputs/$experiment_name/${filename}_vanilla_out.txt | grep 'real:' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?');
@@ -65,7 +66,7 @@ fi
 
 if [ -n "$script_with_PL" ] && ([ "$status_withoutPL" == "ok" ] || [ "$checkpreviousstep" == "no" ]); then
   # run solver with prooflogging
-  $loc_bin/runlim -r $TIMEOUT_SOLVER -s $MEMOUT_SOLVER -o $loc_outputs/$experiment_name/${filename}_pl_out.txt $script_with_PL $instances/${filename}.${extension} $loc_proofs/$experiment_name/${filename}_proof.pbp > $loc_outputs/$experiment_name/${filename}_pl_solveroutput.txt 2>&1
+  $loc_bin/runlim -r $TIMEOUT_SOLVER -s $MEMOUT_SOLVER -o $loc_outputs/$experiment_name/${filename}_pl_out.txt $script_with_PL $instance $loc_proofs/$experiment_name/${filename}_proof.pbp > $loc_outputs/$experiment_name/${filename}_pl_solveroutput.txt 2>&1
   
   # extract results
   res_runtime_withPL=$(cat $loc_outputs/$experiment_name/${filename}_pl_out.txt | grep 'real:' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?');
@@ -87,7 +88,7 @@ if [ -n "$script_with_PL" ] && ([ "$status_withoutPL" == "ok" ] || [ "$checkprev
     answer_withPL="NONE"
   fi
 elif [ "$status_withoutPL" != "ok" ]; then
-  status_withPL=$status_withoutPL
+  status_withPL="notok"
 else 
   status_withPL=ok
 fi
@@ -95,7 +96,7 @@ fi
 if [ "$checkproof" == "yes" ] && ([ "$status_withPL" == "ok" ] || [ "$checkpreviousstep" == "no" ]); then
   # run proof checker
   source $loc_bin/pyenv/bin/activate
-  $loc_bin/runlim -r $TIMEOUT_VERIPB -s $MEMOUT_VERIPB -o $loc_outputs/$experiment_name/${filename}_verification.txt python -m veripb --checkDeletion --wcnf $instances/${filename}.${extension} $loc_proofs/$experiment_name/${filename}_proof.pbp > $loc_outputs/$experiment_name/${filename}_veripb_output.txt 2>&1
+  $loc_bin/runlim -r $TIMEOUT_VERIPB -s $MEMOUT_VERIPB -o $loc_outputs/$experiment_name/${filename}_verification.txt python -m veripb --checkDeletion --wcnf $instance $loc_proofs/$experiment_name/${filename}_proof.pbp > $loc_outputs/$experiment_name/${filename}_veripb_output.txt 2>&1
 
   # extract results
   res_runtime_proofchecker=$(cat $loc_outputs/$experiment_name/${filename}_verification.txt | grep 'real:' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?');
@@ -106,7 +107,7 @@ if [ "$checkproof" == "yes" ] && ([ "$status_withPL" == "ok" ] || [ "$checkprevi
     res_proofcheck_succeeded=1
   else
         res_proofcheck_succeeded=0	
-        # cp $instances/${filename}.${extension} $loc_verification_failed
+        # cp $instance $loc_verification_failed
         # cp $loc_outputs/${filename}_vanilla.txt $loc_verification_failed
         # cp $loc_outputs/${filename}_verification.txt $loc_verification_failed
         # cp $loc_proofs/${filename}_proof.pbp $loc_verification_failed
@@ -127,5 +128,6 @@ fi
 echo "$resultline" >> $loc_results/$experiment_name/"$filename"_result.csv
 
 # rm $loc_outputs/${filename}*
+rm $instance
 rm $loc_proofs/$experiment_name/${filename}*
 rm $loc_running_scripts/$experiment_name/${filename}*
