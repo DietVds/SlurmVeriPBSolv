a0e1d2ec62191876c9ccf0468e92b7c72267600f
DietVds <vandesande.dieter@gmail.com>
2025-07-19 11:06:29 +0200

    update analyse script for situation where only overhead is measured

--- analysis/analyse_complete_workflow.R
+++ analysis/analyse_complete_workflow.R
@@ -77,17 +77,19 @@ for (row in 1:nrow(data)) {
   }
 }
 
-# VeriPB OOTs
-for (row in 1:nrow(data)) {
-  if (!is.na(data[row, "runtime_proofchecker"]) & data[row, "runtime_proofchecker"] >= 10 * time_limit) {
-    data[row, "runtime_proofchecker"] <- 46000
+if ("runtime_proofchecker" %in% colnames(data)){
+  # VeriPB OOTs
+  for (row in 1:nrow(data)) {
+    if (!is.na(data[row, "runtime_proofchecker"]) & data[row, "runtime_proofchecker"] >= 10 * time_limit) {
+      data[row, "runtime_proofchecker"] <- 46000
+    }
   }
-}
 
-# VeriPB OOMs
-for (row in 1:nrow(data)) {
-  if (!is.na(data[row, "mem_proofchecker"]) & data[row, "mem_proofchecker"] >= 2 * mem_limit) {
-    data[row, "runtime_proofchecker"] <- 99000
+  # VeriPB OOMs
+  for (row in 1:nrow(data)) {
+    if (!is.na(data[row, "mem_proofchecker"]) & data[row, "mem_proofchecker"] >= 2 * mem_limit) {
+      data[row, "runtime_proofchecker"] <- 99000
+    }
   }
 }
 
@@ -99,6 +101,7 @@ for (row in 1:nrow(data)) {
 
 # drop qmaxsat OoT/OoM
 solved_by_solver_withoutPL <- data[data$answer_withoutPL != " NONE", ]
+solved_by_solver_withPL <- data[data$answer_withoutPL != "NONE" & data$answer_withPL != "NONE", ]
 
 #ggplot(solved_by_solver_withoutPL, aes(x = runtime_w, y = runtime, color = log10((proofsize / 10^3) + 1))) +
 ggplot(solved_by_solver_withoutPL, aes(x = runtime_withPL, y = runtime_withoutPL)) +
@@ -193,42 +196,44 @@ ggsave(paste(opt.output, opt.expname, "_mem_without_vs_with_PL.pdf", sep=""), de
 
 # TYPE2
 
-# drop qmaxsatpb OoT/OoM
-solved_by_solver_withPL <- data[data$answer_withoutPL != "NONE" & data$answer_withPL != "NONE", ]
 
-#ggplot(solved_by_solver_withPL, aes(x = runtime_v, y = runtime_w, color = log10((proofsize / 10^3) + 1))) +
-ggplot(solved_by_solver_withPL, aes(x = runtime_proofchecker, y = runtime_withPL)) +
-  geom_point(color='darkblue', shape=4) +
-  scale_x_log10(breaks = c(1, 10, 100, 1000, 10000)) +
-  scale_y_log10(breaks = c(1, 10, 100, 1000, 10000)) +
-  #scale_color_continuous(breaks = c(2, 3, 4, 5, 6, 7, 8), labels = c("100KB", "1MB", "10MB", "100MB", "1GB", "10GB", "100GB"), high="#132B43", low="#56B1F7") +
-  coord_fixed(ratio = 1) +
-  geom_vline(xintercept = 46000, linetype = "dashed") +
-  geom_vline(xintercept = 99000, linetype = "dashed") +
-  geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
-  xlab("VeriPB (time in s)") +
-  ylab("Solving with PL (time in s)") +
-  #labs(color = "Proofsize") +
-  coord_cartesian(xlim = c(0.1, 100000), ylim = c(0.1, 100000)) +
-  annotate(
-    geom = "text",
-    label = "OoT",
-    x = 49000,
-    y = 0.081,
-    angle = 90,
-    vjust = 1,
-    size = 2
-  ) +
-  annotate(
-    geom = "text",
-    label = "OoM",
-    x = 105000,
-    y = 0.081,
-    angle = 90,
-    vjust = 1,
-    size = 2
-  )
-ggsave(paste(opt.output, opt.expname, "_solving_vs_checking.pdf", sep=""), device = "pdf", width = 14, height = 12, units = "cm")
+if("runtime_proofchecker" %in% colnames(data)){
+  # drop qmaxsatpb OoT/OoM
+
+  #ggplot(solved_by_solver_withPL, aes(x = runtime_v, y = runtime_w, color = log10((proofsize / 10^3) + 1))) +
+  ggplot(solved_by_solver_withPL, aes(x = runtime_proofchecker, y = runtime_withPL)) +
+    geom_point(color='darkblue', shape=4) +
+    scale_x_log10(breaks = c(1, 10, 100, 1000, 10000)) +
+    scale_y_log10(breaks = c(1, 10, 100, 1000, 10000)) +
+    #scale_color_continuous(breaks = c(2, 3, 4, 5, 6, 7, 8), labels = c("100KB", "1MB", "10MB", "100MB", "1GB", "10GB", "100GB"), high="#132B43", low="#56B1F7") +
+    coord_fixed(ratio = 1) +
+    geom_vline(xintercept = 46000, linetype = "dashed") +
+    geom_vline(xintercept = 99000, linetype = "dashed") +
+    geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
+    xlab("VeriPB (time in s)") +
+    ylab("Solving with PL (time in s)") +
+    #labs(color = "Proofsize") +
+    coord_cartesian(xlim = c(0.1, 100000), ylim = c(0.1, 100000)) +
+    annotate(
+      geom = "text",
+      label = "OoT",
+      x = 49000,
+      y = 0.081,
+      angle = 90,
+      vjust = 1,
+      size = 2
+    ) +
+    annotate(
+      geom = "text",
+      label = "OoM",
+      x = 105000,
+      y = 0.081,
+      angle = 90,
+      vjust = 1,
+      size = 2
+    )
+  ggsave(paste(opt.output, opt.expname, "_solving_vs_checking.pdf", sep=""), device = "pdf", width = 14, height = 12, units = "cm")
+}
 
 resultfile <- paste(opt.output, opt.expname, "_results.txt", sep="")
 
@@ -238,20 +243,24 @@ vanilla.solved <- length(which(data$answer_withoutPL != " NONE"))
 solverpl.solved <- length(which(data$answer_withoutPL != " NONE" & data$answer_withPL != " NONE"))
 solverpl.oot <- length(which(data$runtime_withPL == 4000))
 solverpl.oom <-length(which(data$runtime_withPL == 8500))
-verif.success <- length(which(data$proofcheck_succeeded == 1))
-verif.failed <-length(which(data$runtime_proofchecker < 10*time_limit & data$proofcheck_succeeded == 0))
-verif.oot <- length(which(data$runtime_proofchecker == 46000))
-verif.oom <- length(which(data$runtime_proofchecker == 99000))
 verif.checksum_failed <- length(which(data$checksum_withoutPL != data$checksum_withPL))
+if("runtime_proofchecker" %in% colnames(data)){
+  verif.success <- length(which(data$proofcheck_succeeded == 1))
+  verif.failed <-length(which(data$runtime_proofchecker < 10*time_limit & data$proofcheck_succeeded == 0))
+  verif.oot <- length(which(data$runtime_proofchecker == 46000))
+  verif.oom <- length(which(data$runtime_proofchecker == 99000))
+}
 
 solverpl.slowdown <- round(median(solved_by_solver_withPL$runtime_withPL/solved_by_solver_withPL$runtime_withoutPL, na.rm = TRUE), digits=2)
 solverpl.memoryoverhead <- round(median(solved_by_solver_withPL$mem_withPL/solved_by_solver_withPL$mem_withoutPL, na.rm = TRUE), digits=2)
 solverpl.90thquantile <- round(unname(quantile(solved_by_solver_withPL$runtime_withPL/solved_by_solver_withPL$runtime_withoutPL, probs=0.90, na.rm = TRUE)), digits=2)
 solverpl.90thquantilemem <- round(unname(quantile(solved_by_solver_withPL$mem_withPL/solved_by_solver_withPL$mem_withoutPL, probs=0.90, na.rm = TRUE)), digits=2)
 
-verified <- solved_by_solver_withPL[which(solved_by_solver_withPL$runtime_proofchecker < 46000),]
-verif.slowdown <- round(median(verified$runtime_proofchecker/verified$runtime_withPL, na.rm = TRUE), digits=2)
-verif.90thquantile <- round(unname(quantile(verified$runtime_proofchecker/verified$runtime_withPL, probs=0.90, na.rm = TRUE)), digits=2)
+if("runtime_proofchecker" %in% colnames(data)){
+  verified <- solved_by_solver_withPL[which(solved_by_solver_withPL$runtime_proofchecker < 46000),]
+  verif.slowdown <- round(median(verified$runtime_proofchecker/verified$runtime_withPL, na.rm = TRUE), digits=2)
+  verif.90thquantile <- round(unname(quantile(verified$runtime_proofchecker/verified$runtime_withPL, probs=0.90, na.rm = TRUE)), digits=2)
+}
 
 text.results <- c(text.results, paste("OoT vanila: ",vanilla.oot))
 text.results <- c(text.results, paste("OoM vanila: ",vanilla.oom))
@@ -263,12 +272,15 @@ text.results <- c(text.results, paste("Median slowdown with prooflogging: " , so
 text.results <- c(text.results, paste("Median memory overhead with prooflogging: " , solverpl.memoryoverhead))
 text.results <- c(text.results, paste("90th Quantile slowdown with prooflogging: " , solverpl.90thquantile))
 text.results <- c(text.results, paste("90th Quantile memory overhead with prooflogging: " , solverpl.90thquantilemem))
-text.results <- c(text.results, paste("Verification Success: " ,verif.success))
-text.results <- c(text.results,  paste("Verification Failed: " ,verif.failed))
-text.results <- c(text.results, paste("OoT Verification: " , verif.oot))
-text.results <- c(text.results, paste("OoM Verification: ", verif.oom))
-text.results <- c(text.results, paste("Median slowdown Verification: " , verif.slowdown))
-text.results <- c(text.results, paste("90th Quantile slowdown Verification: " , verif.90thquantile))
+
+if("runtime_proofchecker" %in% colnames(data)){
+  text.results <- c(text.results, paste("Verification Success: " ,verif.success))
+  text.results <- c(text.results,  paste("Verification Failed: " ,verif.failed))
+  text.results <- c(text.results, paste("OoT Verification: " , verif.oot))
+  text.results <- c(text.results, paste("OoM Verification: ", verif.oom))
+  text.results <- c(text.results, paste("Median slowdown Verification: " , verif.slowdown))
+  text.results <- c(text.results, paste("90th Quantile slowdown Verification: " , verif.90thquantile))
+}
 text.results <- c(text.results, paste("Instances checksum failed: ", verif.checksum_failed))
 
 writeLines(text.results, resultfile)
