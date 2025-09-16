e6869dc0445b3f93f21ff42fe827a38f75b74267
DietVds <vandesande.dieter@gmail.com>
2025-09-16 10:30:04 +0200

    Update modules to load

--- helper/load_modules.sh
+++ helper/load_modules.sh
@@ -1,9 +1,4 @@
-# module load binutils/2.36.1-GCCcore-10.3.0
-# module load CMake/3.20.1-GCCcore-10.3.0
-# module load Python/3.9.5-GCCcore-10.3.0
-# module load parallel/20210622-GCCcore-10.3.0
-
-module load binutils/2.38-GCCcore-11.3.0
-module load CMake/3.23.1-GCCcore-11.3.0
-module load Python/3.10.4-GCCcore-11.3.0
-module load parallel/20220722-GCCcore-11.3.0
+module load binutils/2.42-GCCcore-13.3.0
+module load CMake/3.29.3-GCCcore-13.3.0
+module load Python/3.12.3-GCCcore-13.3.0
+module load parallel/20240722-GCCcore-13.3.0
